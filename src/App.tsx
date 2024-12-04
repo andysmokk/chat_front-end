d3dd6281dbff74494d4f6d9bbd221a1b3b14724f
Andrii Konovalov <andy.smokk@gmail.com>
2024-12-04 17:12:38 +0100

    added socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from "react";
 import "./App.css";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3005");
 
 function App() {
-  return <div className="container">HOLA MUNDO</div>;
+  return (
+    <div className="container">
+      <input placeholder="Message" />
+      <button>Send message</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/types.d.ts
@@ -0,0 +1,5 @@
+// src/types.d.ts
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
