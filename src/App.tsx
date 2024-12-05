d40cf7faa36cd193c37cc306a90562b813935d7e
Andrii Konovalov <andy.smokk@gmail.com>
2024-12-05 16:37:14 +0100

    added drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import io from "socket.io-client";
+import Main from "./components/Main/Main";
 
 const socket = io("http://localhost:3005");
 
 function App() {
   return (
-    <div className="container">
-      <input placeholder="Message" />
-      <button>Send message</button>
+    <div>
+      <Main />
     </div>
   );
 }
